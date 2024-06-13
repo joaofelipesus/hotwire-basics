c3b7ac85faf1610647506772c6fee682d3629cb9
joaofelipe <joaofelipe1294@gmail.com>
2024-06-13 08:58:22 -0300

    feat: add model validations and weight class

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,2 +1,14 @@
 class Fighter < ApplicationRecord
+  validates :name, :weight_class, :fight_record, presence: true
+
+  enum weight_class: {
+    flyweight: 'Flyweight',
+    bantamweight: 'Bantamweight',
+    featherweight: 'Featherweight',
+    lightweight: 'Lightweight',
+    welterweight: 'Welterweight',
+    middleweight: 'Middleweight',
+    light_heavyweight: 'Light Heavyweight',
+    heavyweight: 'Heavyweight'
+  }
 end
--- test/models/fighter_test.rb
+++ test/models/fighter_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class FighterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should save valid fighter' do
+    fighter = Fighter.new(name: 'Daniel Cormier', weight_class: 'Heavyweight', fight_record: '22-3-0')
+
+    assert fighter.save
+  end
+
+  test 'should not save fighter without name' do
+    fighter = Fighter.new(weight_class: 'Heavyweight', fight_record: '22-3-0')
+
+    assert_not fighter.save
+    assert fighter.errors.full_messages.include?("Name can't be blank")
+  end
+
+  test 'should not save fighter without weight class' do
+    fighter = Fighter.new(name: 'Daniel Cormier', fight_record: '22-3-0')
+
+    assert_not fighter.save
+    assert fighter.errors.full_messages.include?("Weight class can't be blank")
+  end
+
+  test 'should not save fighter without fight record' do
+    fighter = Fighter.new(name: 'Daniel Cormier', weight_class: 'Heavyweight')
+
+    assert_not fighter.save
+    assert fighter.errors.full_messages.include?("Fight record can't be blank")
+  end
 end
