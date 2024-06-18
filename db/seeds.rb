6c9afd9b832feed9f8ec8933a80ff70b1065f03d
joaofelipe <joaofelipe1294@gmail.com>
2024-06-18 08:07:46 -0300

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Fighter.find_or_create_by(name: 'Demetrious Johnson', weight_class: 'flyweight', fight_record: '27-3-1')
+Fighter.find_or_create_by(name: 'Daniel Cormier', weight_class: 'heavyweight', fight_record: '22-1-0')
+Fighter.find_or_create_by(name: 'Khabib Nurmagomedov', weight_class: 'lightweight', fight_record: '28-0-0')
+Fighter.find_or_create_by(name: 'Jon Jones', weight_class: 'light_heavyweight', fight_record: '26-1-0')
+Fighter.find_or_create_by(name: 'Kamaru Usman', weight_class: 'welterweight', fight_record: '17-1-0')
+Fighter.find_or_create_by(name: 'Anderson Silva', weight_class: 'middleweight', fight_record: '34-10-0')
+Fighter.find_or_create_by(name: 'Max Holloway', weight_class: 'featherweight', fight_record: '21-6-0')
+Fighter.find_or_create_by(name: 'B.J. Penn', weight_class: 'lightweight', fight_record: '16-14-2')
+Fighter.find_or_create_by(name: 'Conor McGregor', weight_class: 'lightweight', fight_record: '22-5-0')
+Fighter.find_or_create_by(name: 'José Aldo Jr.', weight_class: 'bantamweight', fight_record: '21-4-0')
